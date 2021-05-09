03b688e6cc09584afe08a8a4831dffabccb8f6ec
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-09 16:14:28 +0430

    some tests added to users table

---
+++ insert_tests.sql
@@ -0,0 +1,38 @@
+INSERT INTO users (
+	username,
+    password,
+    Fname,
+    Lname,
+    birth_day,
+    register_date,
+    bio
+)
+VALUES 
+	('mahdi79', '1234', 
+     'mahdi', 'malmasi', 
+     '2000-01-01', '2020-01-01',
+     'learning sql')
+     ,
+     ('ae2079', '4231', 
+     'ali', 'ebrahimi', 
+     '2001-01-01', '2021-01-01',
+     DEFAULT)
+     ,
+     ('am80', '4321', 
+     'amir', 'morteza zadeh', 
+     '2001-05-23', '2019-06-05',
+     'new to tutiter')
+     ,
+     ('alisaz', '9876', 
+     'alireza', 'mohseni', 
+     '2001-11-10', '2018-01-01',
+     'song producer')
+     ,
+     ('nvdmsv', '6789', 
+     'navid', 'musavi zadeh', 
+     '2000-01-01', '2020-01-01',
+     'giutarist')
+;
+
+
+
