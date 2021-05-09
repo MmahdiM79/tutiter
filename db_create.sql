87cf65584201db152fe514c4dc0b1f9c3038a835
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-09 15:39:40 +0430

    users table added

--- db_create.sql
+++ db_create.sql
@@ -1,1 +1,20 @@
-CREATE DATABASE `tutiter`; 
+CREATE DATABASE `tutiter`;
+
+
+
+
+
+-- create users table 
+CREATE TABLE `user` (
+	`id` INT(20) AUTO_INCREMENT,
+    `username` VARCHAR(20) NOT NULL,
+    `password` VARCHAR(128) NOT NULL,
+    `Fname` VARCHAR(20) NOT NULL,
+    `Lname` VARCHAR(20) NOT NULL,
+    `birth_day` DATE NOT NULL,
+    `register_date` DATE NOT NULL,
+    `bio` varchar(64) DEFAULT NULL,
+    
+    PRIMARY KEY(`id`, `username`)
+);
+ 
