780ed3e6e0951d61c0626473c3ee110c4f622f41
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-10 15:51:01 +0430

    sign_up.sql file added

---
+++ sign_up.sql
@@ -0,0 +1,23 @@
+
+
+-- first check that the user is exist or not
+-- given user id: kasra80
+SELECT *
+FROM users u
+WHERE u.username = 'kasra80';
+
+
+
+-- now add the new user
+INSERT INTO users (
+	username,
+    password,
+    Fname,
+    Lname,
+    birth_day,
+    register_date,
+    bio
+)
+VALUE ('kasra80', '4567', 'kasra', 'zarei', '2001-04-23', current_date(), DEFAULT);
+
+
