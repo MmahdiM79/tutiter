ceb7d40f768f68d004366a97bc7bfef9ed5a5d36
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-10 10:16:05 +0430

    follows table added

--- db_create.sql
+++ db_create.sql
@@ -72,3 +72,16 @@ CREATE TABLE `blocked` (
     FOREIGN KEY (`user1`) REFERENCES `users` (`id`),
     FOREIGN KEY (`user2`) REFERENCES `users` (`id`)
 );
+
+
+
+
+-- create follows table
+CREATE TABLE `follows` (
+	`user1` INT NOT NULL,
+    `user2` INT NOT NULL,
+    
+    PRIMARY KEY (`user1`, `user2`),
+    FOREIGN KEY (`user1`) REFERENCES `users` (`id`),
+    FOREIGN KEY (`user2`) REFERENCES `users` (`id`)
+);
