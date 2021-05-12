93e4037b12488e4eff47dc87f4c9e20f75f48bc4
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-12 15:08:18 +0430

    block sql file updated"

--- block.sql
+++ block.sql
@@ -8,5 +8,15 @@ VALUE
 	(userID('kasra80'), userID('mahdi79'))
 ;
 	
-    
-    
+
+-- delete mahdi79 from kasra80 following list
+DELETE FROM follow 
+WHERE user1 = userID('kasra80') and user2 = userID('mahdi79');
+
+
+-- delete kasra80 from mahdi79 following list
+DELETE FROM follow 
+WHERE user2 = userID('mahdi79') and user1 = userID('kasra80');
+
+
+	
