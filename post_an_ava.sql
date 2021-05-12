f14b01066d5e596b4cd96b69575338450d2fefba
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-12 14:51:56 +0430

    userID function used

--- post_an_ava.sql
+++ post_an_ava.sql
@@ -3,7 +3,7 @@
 -- post a new ava in tutiter
 INSERT INTO avas (sender_id, ava)
 VALUE (
-	(SELECT id FROM users WHERE username = 'kasra80'),
+	userID('kasra80'),
     'avalin post man tu tutiter'
 );
 
