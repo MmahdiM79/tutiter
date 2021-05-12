063a7360e5cb0f047b124dcd6345b283c7409ba6
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-12 14:52:58 +0430

    userID function used

--- unfollow.sql
+++ unfollow.sql
@@ -4,9 +4,7 @@
 -- kasra80 unfollow mahdi79
 DELETE FROM follow 
 WHERE 
-	user1 =  (SELECT id FROM users WHERE username = 'kasra80')
-    and 
-    user2 = (SELECT id FROM users WHERE username = 'mahdi79')
+	user1 =  userID('kasra80') and userID('mahdi79')
 ;
 
 
