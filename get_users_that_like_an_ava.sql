4ce38b1fff877100e6098bcf587dc98685d3c662
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-28 21:23:36 +0430

    get users that like an ava query added

---
+++ get_users_that_like_an_ava.sql
@@ -0,0 +1,19 @@
+
+
+
+
+SELECT * FROM blocked 
+WHERE user1 = avaSenderID(3) AND user2 = userID('am80');
+
+
+
+SELECT userNAME(l.user_id) as liker
+FROM likes l
+WHERE l.ava_id = 3 
+	AND l.user_id NOT IN 
+		(select user1 from blocked where user2 = userID('am80')) 
+	
+
+
+
+
