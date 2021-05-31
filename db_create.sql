e31eae1a387b169a8c8db6374a165cef12853f4b
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-05-31 22:20:51 +0430

    blocked table changed

--- db_create.sql
+++ db_create.sql
@@ -75,12 +75,13 @@ CREATE TABLE `avas_hashtags` (
 
 
 
+
 -- create blocked tabel
 CREATE TABLE `blocked` (
 	`user1` INT NOT NULL,
     `user2` INT NOT NULL,
     
-    PRIMARY KEY (`user1`, `user2`),
+
     FOREIGN KEY (`user1`) REFERENCES `users` (`id`),
     FOREIGN KEY (`user2`) REFERENCES `users` (`id`)
 );
