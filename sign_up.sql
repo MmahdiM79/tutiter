954fb01f03bf432010eb17053f5e0bab8c3c0a38
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-02 10:56:38 +0430

    minor changes

--- sign_up.sql
+++ sign_up.sql
@@ -2,6 +2,7 @@
 
 -- first check that the user is exist or not
 -- given user id: kasra80
+-- the resualt of this query is null we will go to the next query
 SELECT *
 FROM users u
 WHERE u.username = 'kasra80';
@@ -16,6 +17,6 @@ INSERT INTO users (
     Lname,
     birth_day
 )
-VALUE ('kasra80', '4567', 'kasra', 'zarei', '2001-04-23');
+VALUE ('kasra80', SHA1('4567'), 'kasra', 'zarei', '2001-04-23');
 
 
