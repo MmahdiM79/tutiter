cbd2b664051c29000820cc093a362e94b9b14316
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-02 17:02:42 +0430

    queries updated

--- get_avas_of_specific_hashtag.sql
+++ get_avas_of_specific_hashtag.sql
@@ -3,13 +3,15 @@
 
 
 -- alisaz search for avas with price hashtag
-SELECT DISTINCT userNAME(sender_id) as sender, ava
+
+SELECT DISTINCT userNAME(sender_id) as sender, ava, 'price' as hashtag
 FROM avas a
 LEFT OUTER JOIN blocked b
 	ON a.sender_id = b.user1
-JOIN hashtags h
+JOIN avas_hashtags h
 	ON a.id = h.ava_id
-WHERE h.hashtag = 'price' and (b.user2 != userID('alisaz') OR b.user2 IS NULL)
+WHERE h.hashtag_id = hashtagID('price') AND 
+	  (b.user2 != userID('alisaz') OR b.user2 IS NULL)
 
 
 
