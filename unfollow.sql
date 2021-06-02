6e4b3cf610c269a749b97eac7393601f2787effd
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-02 11:27:32 +0430

    minor changes

--- unfollow.sql
+++ unfollow.sql
@@ -1,10 +1,16 @@
 
 
 
+-- check that kasra80 is following ae2079 or not
+SELECT *
+FROM follow
+WHERE user1 = userID('kasra80') AND user2 = userID('ae2079');
+
+
 -- kasra80 unfollow mahdi79
 DELETE FROM follow 
 WHERE 
-	user1 =  userID('kasra80') and userID('mahdi79')
+	user1 =  userID('kasra80') and userID('ae2079')
 ;
 
 
