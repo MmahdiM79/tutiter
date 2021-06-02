907620c64a1ca7206d0b5b57c054ba8048f650dd
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-02 15:23:39 +0430

    minor changes

--- get_avas_of_following_users.sql
+++ get_avas_of_following_users.sql
@@ -1,14 +1,24 @@
 
 
 
+
+
+
 -- get posts of mahdi79 following users
 -- ordered by date
 
 SELECT userNAME(sender_id) as sender, ava, write_date
+
 FROM avas a
 JOIN follow f
 	ON f.user2 = a.sender_id
-WHERE f.user1 = userID('mahdi79')
+JOIN blocked b
+	ON a.sender_id = b.user1
+    
+WHERE f.user1 = userID('mahdi79') AND b.user2 != userID('mahdi79')
+
 ORDER BY write_date DESC;
+
+
 
 
