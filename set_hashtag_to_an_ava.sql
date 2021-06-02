834faeed32d2ca995b60452950e4ea8909489ccb
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-02 16:53:18 +0430

    bugs fixed

--- set_hashtag_to_an_ava.sql
+++ set_hashtag_to_an_ava.sql
@@ -3,9 +3,17 @@
 
 
 
--- add a hashtag to a post
 
-INSERT INTO hashtags VALUE (DEFAULT, '2', 'first');
+-- check that given hashtag is avialable or not
+SELECT * FROM  hashtags WHERE hashtag = 'first';
+
+
+-- add new hashtag to hashtags table
+INSERT INTO hashtags VALUE (DEFAULT, 'first');
+
+
+-- add a hashtag to a post
+INSERT INTO avas_hashtags VALUE ('2', hashtagID('first'));
 
 
 
