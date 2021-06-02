9091446fb98659d13ea033596d57e0cc28251f2b
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-02 11:23:44 +0430

    minor changes

--- follow.sql
+++ follow.sql
@@ -1,6 +1,18 @@
 
 
 
+
+-- check that kasra80 is following mahdi79 and ae2079 or not
+SELECT *
+FROM follow
+WHERE user1 = userID('kasra80') AND user2 = userID('mahdi79');
+
+SELECT *
+FROM follow
+WHERE user1 = userID('kasra80') AND user2 = userID('ae2079');
+
+
+
 -- kasra80 follows mahdi79 
 INSERT INTO follow 
 VALUES 
