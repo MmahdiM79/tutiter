c8fae5bd46475ff5ad251d912fbc2c098527e1a1
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-02 21:25:37 +0430

    send message query added

---
+++ send_message.sql
@@ -0,0 +1,22 @@
+
+
+
+
+
+-- ae2079 wants to send a message to mahdi79
+
+-- check that mahdi79 has blocked ae2079 or not
+SELECT * FROM blocked WHERE user1 = userID('mahdi79') AND user2 = userID('ae2079');
+
+
+-- check that sender of ava with id 6 has blocked mahdi79 or not
+SELECT * FROM blocked WHERE user1 = avaSenderID(6) AND user2 = userID('mahdi79');
+
+
+
+-- add ae2079 message
+INSERT INTO messages VALUE (DEFAULT, userID('ae2079'), userID('mahdi79'), 'ino didi?', '6');
+
+
+
+
