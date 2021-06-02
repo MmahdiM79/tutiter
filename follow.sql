fa75b9d7324bdc3e401d244f49367b178e54dbf8
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-02 11:20:43 +0430

    bugs fixed

--- follow.sql
+++ follow.sql
@@ -4,8 +4,8 @@
 -- kasra80 follows mahdi79 
 INSERT INTO follow 
 VALUES 
-    (userID('kasra80'), userID('mahdi79'),
+    (userID('kasra80'), userID('mahdi79')),
     (userID('kasra80'), userID('ae2079'))
-);
+;
 
 
