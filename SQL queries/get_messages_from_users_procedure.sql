4596725e5810ae30310079e4c8e38e0b05ff506f
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-22 22:11:41 +0430

    get messages from users procedure added

---
+++ SQL queries/get_messages_from_users_procedure.sql
@@ -0,0 +1,24 @@
+
+
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE  `messages_list`()
+BEGIN 
+
+    SET @`doer` = getDOERid(); -- find the doer of this procedure
+
+	-- show messages list
+    SELECT userNAME(sender_id), max(sent_date) as last_message
+    FROM messages m
+    WHERE reciver_id = @doer
+    GROUP BY sender_id
+    ORDER BY last_message DESC;
+
+END;
+
+
+
+
