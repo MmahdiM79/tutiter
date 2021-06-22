149cce7d009918fee18dff74749e69e71bae0bab
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-06-22 21:07:44 +0430

    get comments of an ava procedure added

---
+++ SQL queries/get_comments_of_an_ava_procedure.sql
@@ -0,0 +1,42 @@
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE `comments_of_ava`(IN `ava_id` INT, OUT res BOOL)
+`scope`:
+BEGIN 
+
+    -- check given ava id
+    IF ava_id NOT IN (SELECT id FROM avas)
+    THEN
+        SELECT 'given ava id is invalid!' as `status`;
+        SELECT FALSE INTO res;
+        LEAVE scope;
+    END IF;
+
+
+    SET @`doer` = getDOERid(); -- find the doer of this procedure
+
+
+	-- check that sender of given ava have block the doer or not
+    IF doer IN (SELECT user2 FROM blocked WHERE user1 = avaSenderID(ava_id))
+    THEN
+		SELECT 'sender of given ava have blocked you!' as `status`;
+        SELECT FALSE INTO res;
+        LEAVE scope;
+	END IF;
+    
+    
+    -- show the comments of given ava
+    SELECT DISTINCT userNAME(a.sender_id) as sender, a.ava, a.write_date
+	FROM avas a
+	LEFT OUTER JOIN blocked b
+		ON b.user1 = a.sender_id
+	WHERE a.comment_of = ava_id AND (b.user2 != doer OR b.user2 IS NULL)
+	ORDER BY a.write_date;
+    
+    
+END;
+
+
