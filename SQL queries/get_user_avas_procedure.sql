2639d39d96c9db8b3d988bdc85ac760e8e2f404e
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-05 09:42:19 +0430

    get user avas procedure added

---
+++ SQL queries/get_user_avas_procedure.sql
@@ -0,0 +1,38 @@
+
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE `user_avas` (OUT res BOOL)
+`scope`:
+BEGIN 
+
+    SET @`doer` = getDOERid(); -- find the doer of this procedure
+
+
+    -- check number of doer avas
+    if 0 = (SELECT count(id) FROM avas 
+        WHERE sender_id = @doer GROUP BY sender_id)
+    THEN
+        SELECT 'you have not sent any ava yet!' as `status`;
+        SELECT FALSE INTO res;
+        LEAVE scope;
+    END IF;
+
+
+    -- set resualt
+    SELECT TRUE INTO res;
+
+
+    -- show user avas
+    SELECT ava, write_date
+    FROM avas
+    WHERE sender_id = @doer AND comment_of IS NULL
+    ORDER BY write_date DESC;
+    
+END;
+
+
+
+
