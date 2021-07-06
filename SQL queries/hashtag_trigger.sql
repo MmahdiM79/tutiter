4ba1e6f4dc44a4768f2215b042a5177ac40d1f6d
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-06 11:17:50 +0430

    bugs fixed

--- SQL queries/hashtag_trigger.sql
+++ SQL queries/hashtag_trigger.sql
@@ -10,30 +10,24 @@ BEGIN
 
     -- hold a copy of new added ava
     SET @`cpy` = NEW.ava;
-    insert into loghold value (@cpy, 'ava cpy');
 
 
     `try`: LOOP
 
         SET @`indx` = locate('#', @cpy); -- find the position of '#' symbol
-        insert into loghold value (@indx, '# index');
-        SET @`hashtag` = substr(@cpy, @indx+1, @indx+4); -- find the hashtag
-        insert into loghold value (@hashtag, 'hashtag');
+        SET @`hashtag` = substr(@cpy, (@indx)+1, 5); -- find the hashtag
 	
         
         -- check pattern
         IF (@hashtag REGEXP '^[a-z][a-z][a-z][a-z][a-z]$') 
         THEN
-			insert into loghold value ('if first if', '1st hashtag');
 			-- check that we have add this hashtag befor or not
-			IF @hashtag NOT IN (SELECT hashtag FROM hashtags)
+			IF @hashtag NOT IN (SELECT h.hashtag FROM hashtags h)
             THEN
-				insert into loghold value ('second if', '');
 				-- add new hashtag
                 INSERT INTO hashtags VALUE (DEFAULT, @hashtag);
 			END IF;
             
-            insert into loghold value ('befor add ava hashtag', '');
 			-- add the hashtag of ava
             INSERT INTO avas_hashtags VALUE (NEW.id, hashtagID(@hashtag));
             
@@ -42,17 +36,15 @@ BEGIN
         
 		-- remove added hashtag
         SET @cpy = substr(@cpy, @indx+1, length(@cpy)+2);
-        insert into loghold value (@cpy, 'new cpy');
         
         
         -- check that we have another hashtag or not
         IF locate('#', @cpy) != 0
         THEN
-			insert into loghold value ('repeat loop', '');
 			ITERATE try;
 		END IF;
         
-        insert into loghold value ('leave lopp', '');
+        
 		LEAVE try;
 	END LOOP try;
 
