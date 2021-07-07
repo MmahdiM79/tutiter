34a8546f09820039a2f9dc76c9fcb5997692489a
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-07 15:41:29 +0430

    bug fixed

--- SQL queries/get_messages_from_an_user_procedure.sql
+++ SQL queries/get_messages_from_an_user_procedure.sql
@@ -23,7 +23,7 @@ BEGIN
     -- compare username_id with doer
     IF @doer = userID(username)
     THEN
-        SELECT 'you want to se messages from yourself? :) !' as `status`;
+        SELECT 'you want to see messages from yourself? :) !' as `status`;
         SELECT FALSE INTO res;
 		    LEAVE scope;
     END IF;
