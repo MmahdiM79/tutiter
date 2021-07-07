0b69e0db3aee39e4b4908ff663b23efc84d9368b
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-07 18:33:43 +0430

    little changes

--- SQL queries/get_messages_from_users_procedure.sql
+++ SQL queries/get_messages_from_users_procedure.sql
@@ -11,7 +11,7 @@ BEGIN
     SET @`doer` = getDOERid(); -- find the doer of this procedure
 
 	-- show messages list
-    SELECT userNAME(sender_id), max(sent_date) as last_message
+    SELECT userNAME(sender_id) as username, max(sent_date) as last_message
     FROM messages m
     WHERE reciver_id = @doer
     GROUP BY sender_id
