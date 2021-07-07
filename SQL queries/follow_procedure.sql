dc0848e9e9c76f02be65904edf3ac364cd3bf689
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-07 15:30:26 +0430

    new conditioin added and some changes

--- SQL queries/follow_procedure.sql
+++ SQL queries/follow_procedure.sql
@@ -6,45 +6,54 @@
 DELIMITER //
 
 CREATE PROCEDURE `follow_user`(IN `username` VARCHAR(20), OUT res BOOL)
-`follow_user_scope`:
+`scope`:
 BEGIN 
 
-    SET @user_id = userID(username); -- find the user id of given username
+    SET @`username_id` = userID(username); -- find the user id of given username
 
 
     -- check that given username is valid or not
-    IF @user_id IS NULL 
+    IF @username_id IS NULL 
     THEN
         SELECT 'the given username is invalid!' as `status`;
         SELECT FALSE INTO res;
-        LEAVE follow_user_scope;
+        LEAVE scope;
     END IF;
 
     
     SET @`doer` = getDOERid(); -- find the doer of this procedure
 
 
+    -- compare username_id with doer
+    IF @doer = @username_id
+    THEN
+        SELECT 'you can not follow yourself :) !' as `status`;
+        SELECT FALSE INTO res;
+		LEAVE scope;
+    END IF;
+
+
     -- check that given username have blocked doer or not
-    IF @doer IN (SELECT user2 FROM blocked WHERE user1 = @user_id)
+    IF @doer IN (SELECT user2 FROM blocked WHERE user1 = @username_id)
     THEN
         SELECT 'the given username have blocked you!' as `status`;
         SELECT FALSE INTO res;
     
     -- check that doer have blocked the given username or not
-    ELSEIF @user_id IN (SELECT user2 FROM blocked WHERE user1 = @doer)
+    ELSEIF @username_id IN (SELECT user2 FROM blocked WHERE user1 = @doer)
 	THEN
         SELECT 'you have blocked the given username!' as `status`;
         SELECT FALSE INTO res;
         
 	-- check that doer have followed the given username or not
-    ELSEIF @user_id IN (SELECT user2 FROM follow WHERE user1 = @doer)
+    ELSEIF @username_id IN (SELECT user2 FROM follow WHERE user1 = @doer)
     THEN
 		SELECT 'you have followed this username!' as `status`;
         SELECT TRUE INTO res;
 		
 	-- add the given username to doer following list
     ELSE
-		INSERT INTO follow VALUE (@doer, @user_id);
+		INSERT INTO follow VALUE (@doer, @username_id);
         SELECT 'the given username added to your following list successfully!' as `status`;
         SELECT TRUE INTO res;
 
