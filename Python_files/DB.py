8f5419c15bc8fb30f5d17af64d2be87812e0b546
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-07 13:56:54 +0430

    new methods added

--- Python_files/DB.py
+++ Python_files/DB.py
@@ -7,7 +7,47 @@ class DB(object):
 
 
     def __init__(self) -> None:
-        connection = sql.connect(host='localhost', database='tutiter', user='root', password='12345')
-        
+        '''
+            this object connect to mysql database with this details:\n
+            \thost: localhost, \n
+            \tdatabase: tutiter, \n
+            \tuser: root, \n
+            \tpassword: ********
+        '''
+        self.connection = sql.connect(host='localhost', database='tutiter', user='root', password='12345')
+        self.cursor = self.connection.cursor()
+
+
+
+
+    def login(self, username: str, password: str) -> tuple:
+        ''' output format:  (bool(res), list(status)) '''
+
+        res = self.cursor.callproc('login', (username, password, 0))[2]
+        self.__commit()
+
+        status = []
+        for result in self.cursor.stored_results():
+            status.append(result.fetchall())
+
+        return (res, status)
+
+
+
+
+    def close(self) -> None:
+        ''' at the end of program call this function '''
+
+        self.cursor.close()
+        self.connection.close()
+
+
+
+
+
+
+    def __commit(self) -> None:
+        ''' apply changes to database'''
+        self.connection.commit()
 
         
