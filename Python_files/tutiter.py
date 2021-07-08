dd9f06b1ba7b3ae4be7318723f156b8947143a90
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-08 13:54:03 +0430

    first menu options handled

--- Python_files/tutiter.py
+++ Python_files/tutiter.py
@@ -1,7 +1,49 @@
-import mysql.connector
+from DB import DB
+from GUI import *
+
+# from Python_files.GUI import *
+# from Python_files.DB import DB
+
+
 
 
 
 
 if __name__ == '__main__':
-    pass
+
+    # create an instance of database 
+    db = DB()
+
+    while True:
+        # start app
+        chosen = start()
+        
+    
+        if chosen == 1: # sign up
+            while True:
+                details = sign_up()
+                if details == ():
+                    break
+
+                report = db.sign_up(details[0], details[1], details[2], details[3], details[4] )
+                
+                show_status(report[1])
+
+                if not report[0]:
+                    continue
+                else:
+                    break
+            
+            db.login(details[0], details[1])
+
+
+        elif chosen == 2: # login
+            username, password = login()
+            db.login(username, password)
+
+
+        else: # exit the app
+            reset()
+            exit()
+
+    
