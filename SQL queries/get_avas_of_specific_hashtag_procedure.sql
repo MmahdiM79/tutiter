72a7bfa194c31edc4ae0a66fe54decc94fe0ee8a
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-09 07:16:58 +0430

    procedure updated

--- SQL queries/get_avas_of_specific_hashtag_procedure.sql
+++ SQL queries/get_avas_of_specific_hashtag_procedure.sql
@@ -6,12 +6,25 @@
 
 DELIMITER //
 
-CREATE PROCEDURE `avas_of_hashtag`(IN `hashtag` CHAR(5))
+CREATE PROCEDURE `avas_of_hashtag`(IN `hashtag` CHAR(5), OUT res BOOL)
+`scope`:
 BEGIN 
 
+    -- check pattern of given hashtag
+    IF NOT (hashtag REGEXP '^[a-z][a-z][a-z][a-z][a-z]$') 
+    THEN
+        SELECT 'the given hashtag has wrong pattern!' as `status`;
+        SELECT FALSE INTO res;
+        LEAVE scope;
+    END IF;
+
+        
     SET @`doer` = getDOERid(); -- find the doer of this procedure
 
 
+    -- set result
+    SELECT TRUE INTO res;
+    
     -- show the avas with given hashtag
     SELECT DISTINCT userNAME(sender_id) as sender, ava, a.write_date
     FROM avas a
